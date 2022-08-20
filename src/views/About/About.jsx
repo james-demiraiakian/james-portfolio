00f42b5e134bb1c77360115af35b8dfc3f60e78e
James Demiraiakian <james.armen@gmail.com>
2022-08-20 14:41:34 -0700

    convert display of about page from flex to grid

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -7,52 +7,41 @@ export default function About() {
   return (
     <div className="about">
       <div className="left-bar" />
-      <div className="content-container">
-        <Header />
-        <div className="intro-container">
-          <div className="intro">
-            I am a well rounded person with experience spanning multiple
-            different industries and companies, and have a multitude of diverse
-            interests. I am a very driven and motivated individual, always
-            seeking to improve myself, expand my knowledge and understanding,
-            and learn new things. I throw myself into problems and challenges
-            head first, and can usually come up with a workable solution within
-            a reasonable time frame, and then take the time to refactor that
-            solution into something more efficient.
-          </div>
-        </div>
-        <br />
-        <div className="work-container">
-          <div className="work">
-            A (semi-comprehensive) list of my work experience, most recent
-            first:
-            <ul>
-              <li>Software Engineering TA - Alchemy Code Lab</li>
-              <li>Manufacturing Supervisor - High Life Farms</li>
-              <li>Lab Tech Supervisor - G1 Perez</li>
-              <li>Vehicle Prep - Enterprise</li>
-              <li>Sign Language TA - College of the Desert</li>
-            </ul>
-          </div>
-        </div>
-        <div className="hobby-container">
-          <div className="hobbies">
-            Some of my hobbies and interests include:
-            <ul>
-              <li>Saber Fencing</li>
-              <li>Lightsaber Fencing</li>
-              <li>Disassembling old electronics</li>
-              <li>Tinkering with Mechanics and Electronics</li>
-              <li>PC Building</li>
-              <li>Riding and maintaining my Motorcycle</li>
-            </ul>
-          </div>
-        </div>
-        <div className="quote-container">
-          <Quotes />
-        </div>
+      <Header />
+      <div className="intro">
+        I am a well rounded person with experience spanning multiple different
+        industries and companies, and have a multitude of diverse interests. I
+        am a very driven and motivated individual, always seeking to improve
+        myself, expand my knowledge and understanding, and learn new things. I
+        throw myself into problems and challenges head first, and can usually
+        come up with a workable solution within a reasonable time frame, and
+        then take the time to refactor that solution into something more
+        efficient.
       </div>
+      <div className="work">
+        A (semi-comprehensive) list of my work experience, most recent first:
+        <ul>
+          <li>Software Engineering TA - Alchemy Code Lab</li>
+          <li>Manufacturing Supervisor - High Life Farms</li>
+          <li>Lab Tech Supervisor - G1 Perez</li>
+          <li>Vehicle Prep - Enterprise</li>
+          <li>Sign Language TA - College of the Desert</li>
+        </ul>
+      </div>
+      <div className="hobbies">
+        Some of my hobbies and interests include:
+        <ul>
+          <li>Saber Fencing</li>
+          <li>Lightsaber Fencing</li>
+          <li>Disassembling old electronics</li>
+          <li>Tinkering with Mechanics and Electronics</li>
+          <li>PC Building</li>
+          <li>Riding and maintaining my Motorcycle</li>
+        </ul>
+      </div>
+      <Quotes />
       <div className="right-bar" />
+      <div className="foot-bar" />
     </div>
   );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import HomeImage from '../../components/HomeImage/HomeImage';
 import Header from '../Header/Header';
+import './Home.css';
 
 export default function Home() {
-    return (
-        <div>
-            <Header />
-            Home: Short blurb and a few photos.
-            <HomeImage />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      Home: Short blurb and a few photos.
+      <HomeImage />
+    </div>
+  );
 }
